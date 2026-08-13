ac2b514c689d6677f3823cf01e7a4ba06546d016
Divansh Gaba <chchchgggg2@gmail.com>
2026-08-13 19:59:01 +0530

    fixed sem1

---
+++ Sem-1/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int a = 5;
+    float b = 578.58;
+    double c = 3.145987138557157;
+
+    char d = 'D';
+    char e[] = "helo divansh gaba this is me 1234@";
+    bool f = true;
+    bool g = false;
+
+    printf("The value of f is: %d\nand g is: %d\n", f,g);
+
+    printf("HEY %c and -- %s\n", d,e);
+    printf("The value of a is: %d\nand float val is %.2f \nand also the long float value is %.15lf\n", a,b,c);
+}
---
+++ Sem-1/run.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+
+set SRC=1.c
+set EXE=main.exe
+
+echo ==========================
+echo Compiling %SRC%...
+echo ==========================
+
+gcc "%SRC%" -o "%EXE%"
+
+if errorlevel 1 (
+    echo.
+    echo Compilation failed!
+    pause
+    exit /b 1
+)
+
+echo.
+echo ==========================
+echo Running %EXE%
+echo ==========================
+echo.
+
+"%EXE%"
+
+echo.
+echo ==========================
+echo Program finished.
+echo ==========================
+pause
