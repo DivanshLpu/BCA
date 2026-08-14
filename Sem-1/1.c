a2aae4099f6e2952e059d80b99e68955a916c707
Divansh Gaba <chchchgggg2@gmail.com>
2026-08-14 10:11:00 +0530

    greates and count code

--- Sem-1/1.c
+++ Sem-1/1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 int main() {
     int a = 5;
     float b = 578.58;
@@ -14,4 +15,12 @@ int main() {
 
     printf("HEY %c and -- %s\n", d,e);
     printf("The value of a is: %d\nand float val is %.2f \nand also the long float value is %.15lf\n", a,b,c);
+
+    // =====================================
+    char name[50] = "\0";
+    printf("ENTER YOUR NAME : ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0'; 
+    printf("Hello, %s", name);
+
 }
